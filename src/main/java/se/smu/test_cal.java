00712ba57629121de59f4978cfe3e6f788cdc363
201511052 <limjy0205@naver.com>
2017-04-15 16:48:33 +0900

    #587 ADT 간단 계산기 프로그램 테스트

--- src/main/java/se/smu/test_cal.java
+++ src/main/java/se/smu/test_cal.java
@@ -1,6 +1,50 @@
+계산기 만들기
+
+
+/*숫자 하나 입력하고 '+' '-' '*' '/' 를 입력 받은후
+다시 숫자를 입력받으면 계산한 결과를 출력하기*/
+
+
+import java.util.*;
 
 public class test_cal
     
 {
-   잘 찾아오셨습니다!! 여기다 만드시면 될것 같아요! 
+	public static void main(String[] args) {
+		double num1,num2,result;
+		char operator;
+
+		Scanner a = new Scanner(System.in);
+
+		System.out.println("첫 번째 숫자를 입력해주세요");
+		num1 = a.nextDouble();
+		
+		System.out.println("두 번째 숫자를 입력해주세요");
+		num2 = a.nextDouble();
+
+		System.out.println("연산 식을 입력해주세요(+,-,*,/)");
+		operator = a.next().charAt(0);
+
+		switch(operator){
+
+		case '+' : result=num1+num2;
+		System.out.println(result);
+		break;
+
+		case '-' : result=num1-num2;
+		System.out.println(result);
+		break;
+
+		case '*' : result=num1*num2;
+		System.out.println(result);
+		break;
+
+		case '/' : result=num1/num2;
+		System.out.println(result);
+		break;
+
+		default: System.out.println("error");
+		break;
+		}
+	}
 }
