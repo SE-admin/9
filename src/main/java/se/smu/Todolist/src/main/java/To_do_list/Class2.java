48c9a594c7786c914ca23be14a5ea29348789e25
201211189 <kimrjf1129@gmail.com>
2017-05-20 21:46:08 +0900

    #770 (UC02) To do 항목을 등록한다 - 실제 마감일을 등록 # Class2.java # 일부분 # To do 항목의 실제 마감일을 등록한다.

--- src/main/java/se/smu/Todolist/src/main/java/To_do_list/Class2.java
+++ src/main/java/se/smu/Todolist/src/main/java/To_do_list/Class2.java
@@ -158,7 +158,7 @@ public class Class2 extends JFrame{ //등록
 	         jt4.setText("");
 	         jt5.setText("");
 	      }
-	      else if(b.getText().equals("삽입")){ //마감 기한을 등록
+	      else if(b.getText().equals("삽입")){ //실제 마감일을 등록
 	    	 try{
 	    		 Scanner scan = new Scanner(new File("list.txt"));
 	    		 while(scan.hasNextLine()) { 
